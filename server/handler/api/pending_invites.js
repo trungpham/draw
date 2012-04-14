d91e6fcd764832bb39428cc6bc6a3c4f61912c14
Trung Pham <trung@phamcom.com>
2012-04-13 22:28:49 -0700

    only show pending invite without a match created

--- server/handler/api/pending_invites.js
+++ server/handler/api/pending_invites.js
@@ -3,7 +3,7 @@ var Drawing = require(process.cwd()+'/server/model/drawing');
 
 module.exports = function(req, res){
 
-    Drawing.find({drawer_id: req.session.userId}, ['external_friend'], function (err, docs) {
+    Drawing.where('drawer_id', req.session.userId).where('match_id', null).select('external_friend').run(function (err, docs) {
 
         var externalFriends = _.collect(docs, function(val, key){
             return val.external_friend;
