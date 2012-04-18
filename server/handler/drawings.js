ff69abb2e6434e8af3efe667da1777625ccd6800
Trung Pham <trung@phamcom.com>
2012-04-17 18:59:38 -0700

    set state and turn on invite.

--- server/handler/drawings.js
+++ server/handler/drawings.js
@@ -7,6 +7,8 @@ module.exports = {
     create: function(req, res){
         var drawing = new Drawing(req.body);
         drawing.drawer_id = req.session.userId;
+        drawing.state = 'drawn';
+        drawing.turn = 1;
         drawing.save();
         res.send(drawing);
     }
